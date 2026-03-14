85050923c6ba46e3c05468de61333b0e31e3db83
Daniel Schaefer <git@danielschaefer.me>
2026-03-15 00:11:28 +0800

    Fix date parsing for timestamps without fractional seconds
    Use datetime.fromisoformat instead of manual string splitting, which
    broke on YouTube timestamps like '2016-04-27T11:09:12Z'.
    
    Signed-off-by: Daniel Schaefer <git@danielschaefer.me>

--- youtuberss/jinja_filters.py
+++ youtuberss/jinja_filters.py
@@ -6,22 +6,9 @@ def format_date(date):
 
 
 def youtube_to_date(youtubeString):
-    # youtube '2016-04-27T11:09:12.000Z'
+    # youtube '2016-04-27T11:09:12.000Z' or '2016-04-27T11:09:12Z'
     # desired 'Mon, 25 Apr 2016 19:03:00 GMT'
-
-    dateSplit = youtubeString.split('-')
-    if not dateSplit:
-        return youtubeString
-    year = dateSplit[0]
-    month = dateSplit[1]
-    (day, times) = dateSplit[2].split('T')
-    timeSplit = times.split(':')
-    hour = timeSplit[0]
-    minutes = timeSplit[1]
-    (seconds, timezone) = timeSplit[2].split('.')
-    date = datetime.datetime(int(year), int(month), int(day),
-                             int(hour), int(minutes), int(seconds))
-    return date
+    return datetime.datetime.fromisoformat(youtubeString.replace('Z', '+00:00'))
 
 
 def format_date_string(youtubeString):
