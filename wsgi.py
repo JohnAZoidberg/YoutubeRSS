effd14f8ae1e9600d4378bef6b50254278f0ca91
Daniel Schaefer <git@danielschaefer.me>
2026-03-14 23:14:49 +0800

    Switch to gunicorn, fix config loading
    Read config path from YOUTUBERSS_CONFIG env var (default conf.json)
    instead of request.environ. Add wsgi.py entry point for gunicorn.
    
    Signed-off-by: Daniel Schaefer <git@danielschaefer.me>

---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from youtuberss import app
+
+if __name__ == "__main__":
+    app.run()
--- youtuberss/home.py
+++ youtuberss/home.py
@@ -1,6 +1,11 @@
+import logging
+import os
+
 from flask import Blueprint, render_template, Response, request
 
 from .fetcher import Fetcher
+
+logger = logging.getLogger(__name__)
 
 home_page = Blueprint('home_page', __name__, template_folder='templates')
 
@@ -17,10 +22,15 @@ def serve(fetcher, podcast, playlist, limit):
     return Response(xml, mimetype='text/xml')
 
 
+def _get_fetcher():
+    config_path = os.environ.get('YOUTUBERSS_CONFIG', 'conf.json')
+    return Fetcher(config_path)
+
+
 @home_page.route('/channel/<channelId>', methods=['GET'])
 def serve_channel_podcast(channelId):
     limit = request.args.get("limit")
-    fetcher = Fetcher(request.environ['YOUTUBERSS_CONFIG'])
+    fetcher = _get_fetcher()
     podcast, upload_playlist = fetcher.get_channel_data(channelId)
     return serve(fetcher, podcast, upload_playlist, limit)
 
@@ -28,7 +38,7 @@ def serve_channel_podcast(channelId):
 @home_page.route('/user/<username>', methods=['GET'])
 def serve_user_podcast(username):
     limit = request.args.get("limit")
-    fetcher = Fetcher(request.environ['YOUTUBERSS_CONFIG'])
+    fetcher = _get_fetcher()
     podcast, upload_playlist = fetcher.get_user_data(username)
     return serve(fetcher, podcast, upload_playlist, limit)
 
@@ -36,6 +46,6 @@ def serve_user_podcast(username):
 @home_page.route('/list/<list_id>', methods=['GET'])
 def serve_playlist_podcast(list_id):
     limit = request.args.get("limit")
-    fetcher = Fetcher(request.environ['YOUTUBERSS_CONFIG'])
+    fetcher = _get_fetcher()
     podcast, upload_playlist = fetcher.get_playlist_data(list_id)
     return serve(fetcher, podcast, upload_playlist, limit)
